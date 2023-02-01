fbce662e6ad719bde686702640328a2091f29246
SY-GG <sy.park.HUB@gmail.com>
2023-02-02 00:15:44 +0900

    product 단순 product list 불러오기

---
+++ src/main/java/com/hyundai/thehandsome/mapper/ProductListDAO.java
@@ -0,0 +1,27 @@
+package com.hyundai.thehandsome.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.hyundai.thehandsome.Vo.product.ListVO;
+
+/**
+ * ProductListDAO
+ * @author 박세영
+ * @since 2023.02.01
+ * @version 1.0
+ * 
+ * <pre>
+ * 수정일        	수정자       			수정내용
+ * ----------  --------    ---------------------------
+ * 2023.02.01  	박세영        최초 생성, getProductList() 추가
+ * </pre>
+ */
+
+@Mapper
+public interface ProductListDAO {
+	// Product List 불러오기
+	List<ListVO> getProductList();
+	
+}
