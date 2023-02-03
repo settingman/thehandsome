37da2b06f8fa99514d384da0f7485e33c4c7f838
SY-GG <sy.park.HUB@gmail.com>
2023-02-03 17:18:06 +0900

    product colorVO에 PCID 추가

---
+++ src/main/java/com/hyundai/thehandsome/Vo/product/ColorVO.java
@@ -0,0 +1,12 @@
+package com.hyundai.thehandsome.Vo.product;
+
+import lombok.Data;
+
+@Data
+public class ColorVO {
+	private String PID;
+	private String PCID;
+	private String PCIMG1;
+	private String PCCHIPIMG;
+	private String PCCOLORCODE;
+}
