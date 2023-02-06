6d7d19ff4338e4bff8cc171a868b5ab60f13ab94
SY-GG <sy.park.HUB@gmail.com>
2023-02-06 21:41:45 +0900

    productDetail로 분리, get ProductDetail, getProductImg 추가 mapper test 까지 완료

---
+++ src/test/java/com/hyundai/thehandsome/mapper/product/ProductDetailTest.java
@@ -0,0 +1,45 @@
+package com.hyundai.thehandsome.mapper.product;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.hyundai.thehandsome.Vo.product.detail.ProductDetailVO;
+import com.hyundai.thehandsome.mapper.ProductListDAO;
+
+import lombok.extern.log4j.Log4j2;
+/**
+ * 
+ * ProductDetailTest.java
+ * @author 박세영
+ * @since 2023. 2. 6.
+ * 
+ * <pre>
+ * 수정일         수정자       			  수정내용
+ * ----------  --------    ---------------------------
+ * 2023. 2. 6.  박세영		getProductDetailTest, getProductImgTest 추가       
+ * </pre>
+ */
+@SpringBootTest
+@Log4j2
+public class ProductDetailTest {
+	@Autowired
+	private ProductListDAO pListDAO;
+	
+	@Disabled
+	void getProductDetailTest() {
+		ProductDetailVO info= pListDAO.getProductDetail("CS2D1KTO002WLV_BK");
+		log.info(info);
+	}
+	
+	@Disabled
+	void getProductImgTest() {
+		List<String> imgList= pListDAO.getProductImg("CS2D1KTO002WLV_BK");
+		for(String src : imgList) {			
+			log.info(src);
+		}
+	}
+	
+}
