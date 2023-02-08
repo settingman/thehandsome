477966d7983936713493087ae1923f87687cdaf6
SY-GG <sy.park.HUB@gmail.com>
2023-02-08 16:47:33 +0900

    product detail rest controller : getStock 수정 , service 변수명 일부 수정

--- src/main/java/com/hyundai/thehandsome/controller/product/ProductDetailRestController.java
+++ src/main/java/com/hyundai/thehandsome/controller/product/ProductDetailRestController.java
@@ -7,11 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hyundai.thehandsome.Vo.product.detail.ProductStockVO;
 import com.hyundai.thehandsome.service.ProductListService;
+import com.hyundai.thehandsome.service.product.ProductDetailService;
 
 import lombok.extern.log4j.Log4j2;
 /**
@@ -32,13 +33,28 @@ import lombok.extern.log4j.Log4j2;
 public class ProductDetailRestController {
 
 	@Autowired
-	private ProductListService service;
+	private ProductListService listService;
+	
+	@Autowired
+	private ProductDetailService detailService;
 
 	@GetMapping(value = "/imgList/{PCID}")
 	public ResponseEntity<List<String>> getimg(@PathVariable("PCID") String PCID) {
 		ResponseEntity<List<String>> result = null;
 		try {
-			result = new ResponseEntity<List<String>>(service.getProductImg(PCID), HttpStatus.OK);
+			result = new ResponseEntity<List<String>>(listService.getProductImg(PCID), HttpStatus.OK);
+			log.info(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	@GetMapping(value = "/stock/{PCID}")
+	public ResponseEntity<List<ProductStockVO>> getStock(@PathVariable("PCID") String PCID) {
+		ResponseEntity<List<ProductStockVO>> result = null;
+		try {
+			result = new ResponseEntity<List<ProductStockVO>>(detailService.getStock(PCID), HttpStatus.OK);
 			log.info(result);
 		} catch (Exception e) {
 			e.printStackTrace();
