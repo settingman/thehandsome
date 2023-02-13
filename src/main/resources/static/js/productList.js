ba63dd9574f87e51a24fca45458c29fae303b28b
SY-GG <sy.park.HUB@gmail.com>
2023-02-13 14:49:05 +0900

    product 무한 스크롤

--- src/main/resources/static/js/productList.js
+++ src/main/resources/static/js/productList.js
@@ -37,14 +37,14 @@ function getSecondDepth(url) {
     let depthList = `
     <div class="flag_wrap case swiper-container swiper-container-horizontal swiper-container-free-mode swiper-container-android swiper-container-wp8-horizontal"> 
       <ul class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px);">
-        <li class="swiper-slide flag on swiper-slide-active" id="categoryNav01" style="margin-right: 6px; width:fit-content">
+        <li class="swiper-slide flag on swiper-slide-active" id="categoryNav01" style="margin-right: 6px; widfit-content">
           <a href="/product/ProductList/${url}/" class="name" onclick="GA_Event('카테고리_리스트', '카테고리', '&nbsp;전체');">${url} 전체</a>
         </li>
       `;
 
     $(data).each(function () {
       depthList += `
-        <li class="swiper-slide flag swiper-slide-next" style="margin-right: 6px; width: fit-content;">
+        <li class="swiper-slide flag swiper-slide-next" style="margin-right: 6px; wid fit-content;">
           <a href="/product/ProductList/${url}${this}/" class="name" onclick="GA_Event('카테고리_리스트', '카테고리', '재킷');">${this}</a>
         </li>
         `;
@@ -97,14 +97,14 @@ function getThirdDepth(url) {
     depthList += `
     <div class="flag_wrap case swiper-container depth2 swiper-container-horizontal swiper-container-free-mode swiper-container-android swiper-container-wp8-horizontal"> 
       <ul class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px);">
-        <li class="swiper-slide flag swiper-slide-next on" style="margin-right: 6px; width: fit-content;  ">
+        <li class="swiper-slide flag swiper-slide-next on" style="margin-right: 6px; wid fit-content;  ">
           <a href="/product/ProductList/${depth1}/" class="name" onclick="GA_Event('카테고리_리스트', '카테고리', '재킷');">${depth2} 전체</a>
         </li>
       `;
 
     $(data).each(function () {
       depthList += `
-        <li class="swiper-slide flag swiper-slide-next" style="margin-right: 6px; width: fit-content;  ">
+        <li class="swiper-slide flag swiper-slide-next" style="margin-right: 6px; wid fit-content;  ">
           <a href="/product/ProductList/${depth1}${depth2}${this}/" class="name" onclick="GA_Event('카테고리_리스트', '카테고리', '재킷');">${this}</a>
         </li>
         `;
@@ -136,51 +136,113 @@ function chgHeaderTitle(title) {
   $("#headerTitle").html(headerTitleHtml);
 }
 
+let canLoadNextPage = true; // 일회용 글로벌 변수
 // TODO: 무한 스크롤
 function YesScroll() {
   const pagination = document.querySelector("#listEnd"); // 페이지네이션 정보획득
   const fullContent = document.querySelector("#hsProductList"); // 전체를 둘러싼 컨텐츠 정보획득
   const screenHeight = screen.height; // 화면 크기
-  let oneTime = false; // 일회용 글로벌 변수
+//   let canLoadNextPage = false; // 일회용 글로벌 변수
+
   document.addEventListener("scroll", OnScroll, { passive: true }); // 스크롤 이벤트함수정의
+
   function OnScroll() {
     //스크롤 이벤트 함수
     const fullHeight = fullContent.clientHeight; // listBody 클래스의 높이
     const scrollPosition = pageYOffset; // 스크롤 위치
-    if ((fullHeight-200) - screenHeight / 2 <= scrollPosition && !oneTime) {
-      // 만약 전체높이-화면높이/2가 스크롤포지션보다 작아진다면, 그리고 oneTime 변수가 거짓이라면
-      oneTime = true; // oneTime 변수를 true로 변경해주고,
-      console.log('madeBox')
-      madeBox(); // 컨텐츠를 추가하는 함수를 불러온다.
+    console.log(`${fullHeight} | ${scrollPosition} >> ${canLoadNextPage}`)
+    if (fullHeight - 200 - screenHeight / 2 <= scrollPosition && canLoadNextPage) {
+      // 만약 전체높이-화면높이/2가 스크롤포지션보다 작아진다면, 그리고 canLoadNextPage 변수가 거짓이라면
+      canLoadNextPage = false; // canLoadNextPage 변수를 true로 변경해주고,
+      console.log("madeBox");
+      madeBox();
     }
   }
 }
 
-function madeBox(){
-    console.log("url", window.location.href);
-    var token = $("meta[name='_csrf']").attr("content");
-    var header = $("meta[name='_csrf_header']").attr("content");
-    $.ajax({
-        url: `/productList/getList`,
-         type: "post",
-        beforeSend : function(xhr){
-            xhr.setRequestHeader(header, token);
-        },
-        data : {
-            currentPage:2,
-            categoryCode : 'we051',
-            brand: ""
-        },
-        dataType: "json",
-        error: function (request, status, error) {
-          console.log("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
-        },
-        success: function (result) {
-          result.forEach(el => {
-            console.log(el)
-          });
-        },
-      });
+let currentpage = 2; //다음 로딩할 페이지 글로벌 변수
+function madeBox() {
+  console.log("url", window.location.href);
+  var token = $("meta[name='_csrf']").attr("content");
+  var header = $("meta[name='_csrf_header']").attr("content");
+  $.ajax({
+    url: `/productList/getList`,
+    type: "post",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader(header, token);
+      console.log("-------------------------------------------세팅끝");
+    },
+    data: {
+      currentPage: currentpage,
+      categoryCode: "we051",
+      brand: "",
+    },
+    dataType: "json",
+    error: function (request, status, error) {
+      console.log("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+    },
+    success: function (result) {
+      let itemList = ``;
+      result.forEach(pInfo => {
+        console.log(pInfo);
+        itemList += `
+            <li class="float active" id="listBody1_li8">
+                <a href="/product/ProductDetail/${pInfo.colorList[0].pcid}" class="categoryImgBox" id="|${pInfo.pid}_link|">
+                <div class="img">
+                    <img
+                    src="${pInfo.colorList[0].pcimg1}"
+                    data-original="http://newmedia.thehandsome.com/SJ/2D/SS/SJ2D1WJC361W_BK_T01.jpg"
+                    id="${pInfo.pid}"
+                    alt="${pInfo.pname}"
+                    text="http://newmedia.thehandsome.com/SJ/2D/SS/SJ2D1WJC361W_BK_T01.jpg"
+                    class="respon_image lazy"
+                    style="display: block" />
+                </div>
+                </a>
+                <div class="info_cont">
+                <div class="color_chip">`;
 
-	console.log('Done');
+        pInfo.colorList.forEach((color, index) => {
+          console.log(color);
+          // 컬러칩 더하기
+          itemList += `
+                    <a href="javascript:noLink();" 
+                        data-pid="${pInfo.pid}" 
+                        data-pcid="${color.pcid}"  
+                        onclick="chgColor(this.getAttribute('data-pid'), this.getAttribute('data-pcid'));">
+                        <button
+                            class="${index == 0 ? "chip on" : "chip"}"
+                            id="${color.pcid}"
+                            style="background:${color.pccolorcode} url(${color.pcchipimg})">"
+                        </button>
+                    </a>`;
+        });
+
+        itemList += `</div>
+            <p class="brand">${pInfo.bname}</p>
+            <p class="name">${pInfo.pname}</p>
+            <p class="price" id="listPrice8">
+                <span id="price_SJ2D1WJC361W_BK" text="'₩'+ ${pInfo.pcprice}'"></span>
+            </p>
+            </div>
+
+            <button
+            type="button"
+            class="like"
+            like-data="${pInfo.colorList[0].pcid}"
+            classappend="${pInfo.colorList[0].isLiked} == true ? on"
+            id="${pInfo.pid}+Like"
+            onclick="callWishListClick($(this), this.getAttribute('data-value'), '%uB9B0%uB128%20%uBE14%uB80C%uB4DC%20%uBC84%uD2BC%20%uC5C5%20%uC2F1%uAE00%20%uC7AC%uD0B7', '1','8');"
+            data-value="${pInfo.colorList[0].pcid}">
+            찜하기
+            </button>
+        </li>`;
+      });
+      $("#listBody").append(itemList);
+
+      canLoadNextPage = true; 
+      currentpage ++;
+      console.log("DONE");
+    },
+  });
 }
